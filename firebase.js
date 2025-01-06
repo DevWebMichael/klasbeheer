464912718d7a1f879eefc2acc8fb0c078a4eec57
Devwebmichael <Devwebmichael@gmail.com>
2025-01-06 22:37:35 +0100

    seven

--- firebase.js
+++ firebase.js
@@ -1,30 +1,22 @@
-// Firebase configuratie
+// Import necessary Firebase SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAM1tSOHVaorTbCH1iiADdbZBuIxtqJCbk",
-    authDomain: "klasbeheer.firebaseapp.com",
-    databaseURL: "https://klasbeheer-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "klasbeheer",
-    storageBucket: "klasbeheer.firebasestorage.app",
-    messagingSenderId: "573161343925",
-    appId: "1:573161343925:web:5eb7f6408e9441276fa7fc",
-    measurementId: "G-1340LLBD63"
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const database = firebase.database();
-  
-  // Data ophalen
-  const dbRef = database.ref('klassen');
-  dbRef.get()
-    .then((snapshot) => {
-      if (snapshot.exists()) {
-        console.log(snapshot.val());
-      } else {
-        console.log("No data available");
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  
+  apiKey: "AIzaSyAM1tSOHVaorTbCH1iiADdbZBuIxtqJCbk",
+  authDomain: "klasbeheer.firebaseapp.com",
+  databaseURL: "https://klasbeheer-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "klasbeheer",
+  storageBucket: "klasbeheer.firebasestorage.app",
+  messagingSenderId: "573161343925",
+  appId: "1:573161343925:web:5eb7f6408e9441276fa7fc",
+  measurementId: "G-1340LLBD63"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// Exporting the database and methods for use in other files
+export { database, ref, get };
