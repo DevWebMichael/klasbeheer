909f7def7ea85866d4a77c23c54339976541de4d
Devwebmichael <Devwebmichael@gmail.com>
2025-01-09 15:41:22 +0100

    17

--- firebase.js
+++ firebase.js
@@ -1,22 +1,19 @@
-// Import necessary Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
+import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAM1tSOHVaorTbCH1iiADdbZBuIxtqJCbk",
   authDomain: "klasbeheer.firebaseapp.com",
   databaseURL: "https://klasbeheer-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "klasbeheer",
-  storageBucket: "klasbeheer.firebasestorage.app",
+  storageBucket: "klasbeheer.appspot.com",
   messagingSenderId: "573161343925",
   appId: "1:573161343925:web:5eb7f6408e9441276fa7fc",
   measurementId: "G-1340LLBD63"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-// Exporting the database and methods for use in other files
-export { database, ref, get };
+// Exporteer de benodigde functies en objecten
+export { database, ref, update };
